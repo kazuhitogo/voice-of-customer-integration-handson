fc58e555629084f067bee0579d1bc287d5d2806d
kazuhitogo <kazuhitogo@gmail.com>
2020-10-26 19:17:50 +0900

    deleted unused code

--- functions/source/CheckTranscriptionJob/check_transcribe.py
+++ functions/source/CheckTranscriptionJob/check_transcribe.py
@@ -1,36 +1,4 @@
-from aws_requests_auth.aws_auth import AWSRequestsAuth
-from elasticsearch import Elasticsearch, RequestsHttpConnection
-import os,boto3,certifi,boto3,json
-from time import sleep
-
-REGION = os.getenv('AWS_REGION')
-esendpoint = os.environ['ES_DOMAIN']
-session = boto3.session.Session()
-credentials = session.get_credentials().get_frozen_credentials()
-awsauth = AWSRequestsAuth(
-    aws_access_key=credentials.access_key,
-    aws_secret_access_key=credentials.secret_key,
-    aws_token=credentials.token,
-    aws_host=esendpoint,
-    aws_region=REGION,
-    aws_service='es'
-)
-es = Elasticsearch(
-    hosts=[{'host': esendpoint, 'port': 443}],
-    http_auth=awsauth,
-    use_ssl=True,
-    verify_certs=True,
-    ca_certs=certifi.where(),
-    timeout=120,
-    connection_class=RequestsHttpConnection
-)
-def es_make_body(contact_id):
-    esbody = {
-        "_source": ["Agent"],
-        "query": {
-            "term":{"_id":{"value":contact_id}}}
-    }
-    return esbody
+import boto3,json
 
 # The entry point for the lambda function
 def lambda_handler(event, context):
